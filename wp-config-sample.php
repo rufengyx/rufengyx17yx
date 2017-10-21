bd6610fc023dba11fc573efb5e90aa2d6f9d3d2e
rufengyx <ccinter@gmail.com>
2017-10-22 00:51:33 +0800

    Update wp-config-sample.php

--- wp-config-sample.php
+++ wp-config-sample.php
@@ -55,6 +55,9 @@ define('AUTH_SALT',        'put your unique phrase here');
 define('SECURE_AUTH_SALT', 'put your unique phrase here');
 define('LOGGED_IN_SALT',   'put your unique phrase here');
 define('NONCE_SALT',       'put your unique phrase here');
+define("FS_METHOD", "direct");
+define("FS_CHMOD_DIR", 0777);
+define("FS_CHMOD_FILE", 0777);
 
 /**#@-*/
 
